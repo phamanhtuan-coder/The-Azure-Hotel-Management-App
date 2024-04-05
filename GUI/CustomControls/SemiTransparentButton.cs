3c473be070adaa5526c8c55f427c777638588aa8
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-04-05 15:38:21 +0700

    Add Data Provider Remove unnecessary files

---
+++ GUI/CustomControls/SemiTransparentButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.CustomControls
+{
+    public class SemiTransparentButton : Button
+    {
+
+        public SemiTransparentButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.Transparent;
+            this.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            this.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            this.ImageAlign = ContentAlignment.MiddleCenter;
+            this.TextImageRelation = TextImageRelation.ImageBeforeText;
+            this.ForeColor = Color.White;
+            this.Font = new Font("Montserrat", 10F);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.Clear(this.BackColor);
+        }
+
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.Invalidate();
+        }
+    }
+}
--- GUI/frmLogin.cs
+++ GUI/frmLogin.cs
@@ -10,30 +10,31 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BLL;
 using GUI.customForm;
 
 namespace GUI
 {
-   
+
 
     public partial class frmLogin : Form
     {
+        /*-------------------------------------------------------------------------------------------------------------------
+                                BẮT ĐẦU ĐOẠN KHAI BÁO CÁC BIẾN TOÀN CỤC
+       --------------------------------------------------------------------------------------------------------------------*/
+
         private Timer timerLoop;
         private int gifDuration = 18000;
         private int elapsed = 0;
 
-        private userBLL userBLL;
+        /*-------------------------------------------------------------------------------------------------------------------
+                                    KẾT THÚC ĐOẠN KHAI BÁO CÁC BIẾN TOÀN CỤC
+       --------------------------------------------------------------------------------------------------------------------*/
 
-        public frmLogin()
-        {
-            InitializeComponent();
-            InitializeTimer();
-            this.DoubleBuffered = true;
 
-            userBLL = new userBLL();
-        }
 
+        /*-------------------------------------------------------------------------------------------------------------------
+                                 BẮT ĐẦU ĐOẠN XỬ LÝ ANIMATION CHO BACKGROUND
+        --------------------------------------------------------------------------------------------------------------------*/
         private void InitializeTimer()
         {
             timerLoop = new Timer();
@@ -52,66 +53,14 @@ namespace GUI
                 elapsed %= gifDuration;
             }
         }
-
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            using (customMessageBox messageBox = new customMessageBox("Bạn có muốn thoát chương trình không?"))
-            {
-                DialogResult dr = messageBox.ShowDialog();
-                if (dr == DialogResult.Cancel)
-                {
-                    messageBox.Close();
-                    return;
-                }
-                else if (dr == DialogResult.OK)
-                {
-                    this.Close();
-                    messageBox.Close();
-                }
-            }
-            Application.Exit();
-        }
-
-  
-
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            frmMain mainForm = new frmMain();
-            this.Hide();
-            mainForm.Show();
-            this.Close();
+        /*-------------------------------------------------------------------------------------------------------------------
+                         KẾT THÚC ĐOẠN XỬ LÝ ANIMATION CHO BACKGROUND
+        --------------------------------------------------------------------------------------------------------------------*/
 
 
-            //string username = txtUsername.Text.Trim();
-            //string password = txtPassword.Text.Trim();
-
-            //bool isValidLogin = userBLL.KiemTraDangNhap(username, password);
-
-            //if (isValidLogin)
-            //{
-            //    //MessageBox.Show("Đăng nhập thành công!");
-            //    // Thực hiện các hành động sau khi đăng nhập thành công
-            //    frmMain mainForm = new frmMain();
-            //    mainForm.Show();
-            //    this.Hide();
-            //    this.FormClosing -= frmLogin_FormClosing;
-            //    this.Close();
-            //    this.FormClosing += frmLogin_FormClosing;
-            //}
-            //else
-            //{
-            //    // Đăng nhập thất bại
-            //    //MessageBox.Show("Tên người dùng hoặc mật khẩu không đúng!");
-            //}
-
-        }
-
-        private void btnShowHidePassword_Click(object sender, EventArgs e)
-        {
-           
-            
-        }
-
+        /*-------------------------------------------------------------------------------------------------------------------
+                         BẮT ĐẦU ĐOẠN XỬ LÝ ANIMATION CHO PANEL LOGIN/QUÊN MẬT KHẨU
+        --------------------------------------------------------------------------------------------------------------------*/
         private void linkForget_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             int viTriX = (panLogin.Location.X == 520) ? 60 : 520;
@@ -178,91 +127,95 @@ namespace GUI
                 tmrTransistion.Stop();
             }
         }
+        /*-------------------------------------------------------------------------------------------------------------------
+                KẾT THÚC ĐOẠN XỬ LÝ ANIMATION CHO PANEL LOGIN/QUÊN MẬT KHẨU
+        --------------------------------------------------------------------------------------------------------------------*/
+
+        /*-------------------------------------------------------------------------------------------------------------------
+                 BẮT ĐẦU ĐOẠN XỬ LÝ CHO CÁC BUTTON
+        --------------------------------------------------------------------------------------------------------------------*/
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            using (customMessageBox messageBox = new customMessageBox("Bạn có muốn thoát chương trình không?"))
+            {
+                DialogResult dr = messageBox.ShowDialog();
+                if (dr == DialogResult.Cancel)
+                {
+                    messageBox.Close();
+                    return;
+                }
+                else if (dr == DialogResult.OK)
+                {
+                    this.Close();
+                    messageBox.Close();
+                }
+            }
+            Application.Exit();
+        }
+
+
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            frmMain mainForm = new frmMain();
+            this.Hide();
+            mainForm.Show();
+            this.Close();
+
+
+            //string username = txtUsername.Text.Trim();
+            //string password = txtPassword.Text.Trim();
+
+            //bool isValidLogin = userBLL.KiemTraDangNhap(username, password);
+
+            //if (isValidLogin)
+            //{
+            //    //MessageBox.Show("Đăng nhập thành công!");
+            //    // Thực hiện các hành động sau khi đăng nhập thành công
+            //    frmMain mainForm = new frmMain();
+            //    mainForm.Show();
+            //    this.Hide();
+            //    this.FormClosing -= frmLogin_FormClosing;
+            //    this.Close();
+            //    this.FormClosing += frmLogin_FormClosing;
+            //}
+            //else
+            //{
+            //    // Đăng nhập thất bại
+            //    //MessageBox.Show("Tên người dùng hoặc mật khẩu không đúng!");
+            //}
+
+        }
+
+        private void btnShowHidePassword_Click(object sender, EventArgs e)
+        {
+
+
+        }
 
         private void btnSignUp_Click(object sender, EventArgs e)
         {
             frmDangKy frm = new frmDangKy();
             frm.ShowDialog();
         }
-    }
 
 
-    public class SemiTransparentPanel : Panel
-    {
-        public SemiTransparentPanel()
+        /*-------------------------------------------------------------------------------------------------------------------
+                     KẾT THÚC ĐOẠN XỬ LÝ CHO CÁC BUTTON
+         --------------------------------------------------------------------------------------------------------------------*/
+        /*-------------------------------------------------------------------------------------------------------------------
+                    HÀM KHỞI TẠO FORM LOGIN, CÓ THỂ THÊM CÁC HÀM KHÁC VÀO ĐÂY
+           --------------------------------------------------------------------------------------------------------------------*/
+        /// <summary>
+        /// Khởi tạo form login và các phần tử controller cùng với các timer để chạy hiệu ứng
+        /// </summary>
+        public frmLogin()
         {
-            this.DoubleBuffered = true; 
-            this.SetStyle(ControlStyles.ResizeRedraw, true); 
+            InitializeComponent();
+            InitializeTimer();
+            this.DoubleBuffered = true;
+
         }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-
-            
-            GraphicsPath path = new GraphicsPath();
-            int radius = 20; 
-            int diameter = radius * 2;
-            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
-            Size size = new Size(diameter, diameter);
-            Rectangle arc = new Rectangle(rect.Location, size);
-
-            path.AddArc(arc, 180, 90);
-            arc.X = rect.Right - diameter;
-            path.AddArc(arc, 270, 90);
-            arc.Y = rect.Bottom - diameter;
-            path.AddArc(arc, 0, 90);   
-            arc.X = rect.Left;
-            path.AddArc(arc, 90, 90); 
-            path.CloseFigure();
-
-            
-            Color semiTransparentColor = Color.FromArgb(128, this.BackColor);
-
-            
-            using (SolidBrush brush = new SolidBrush(semiTransparentColor))
-            {
-                e.Graphics.FillPath(brush, path);
-            }
-
-          
-            path.Dispose();
-        }
-
-        protected override void OnSizeChanged(EventArgs e)
-        {
-            base.OnSizeChanged(e);
-            this.Invalidate(); 
-        }
-    }
-
-    public class SemiTransparentButton : Button
-    {
-    
-        public SemiTransparentButton()
-        {
-            this.FlatStyle = FlatStyle.Flat;
-            this.FlatAppearance.BorderSize = 0; 
-            this.BackColor = Color.Transparent; 
-            this.FlatAppearance.MouseOverBackColor = Color.Transparent; 
-            this.FlatAppearance.MouseDownBackColor = Color.Transparent; 
-            this.ImageAlign = ContentAlignment.MiddleCenter; 
-            this.TextImageRelation = TextImageRelation.ImageBeforeText; 
-            this.ForeColor = Color.White; 
-            this.Font = new Font("Segoe UI Semibold", 9F); 
-        }
-
-        protected override void OnPaint(PaintEventArgs pevent)
-        {
-            base.OnPaint(pevent);
-            pevent.Graphics.Clear(this.BackColor);
-        }
-
-
-        protected override void OnSizeChanged(EventArgs e)
-        {
-            base.OnSizeChanged(e);
-            this.Invalidate(); 
-        }
     }
 }
