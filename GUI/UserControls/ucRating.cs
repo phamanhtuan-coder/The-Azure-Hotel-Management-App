f974e95ca4b89d94b7b481946223e2edc1cae7e2
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-04-11 16:02:57 +0700

    add frmDanhGia

--- GUI/UserControls/ucRating.cs
+++ GUI/UserControls/ucRating.cs
@@ -1,4 +1,5 @@
-﻿using Syncfusion.WinForms.ListView;
+﻿using GUI.customForm;
+using Syncfusion.WinForms.ListView;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,95 +15,78 @@ namespace GUI.UserControls
     public partial class ucRating : UserControl
     {
 
+        frmDanhGia frm = new frmDanhGia();
+        customMessageBox thongBao;
         public ucRating()
         {
             InitializeComponent();
         }
 
-
-        private void khoiTaoGiaTrichoForm()
-        {
-    
-            List<string> trangThaiSort = new List<String>();
-            List<string> trangThaiXoa = new List<string>();
-        
-      
-
-            trangThaiSort.Add("Tăng dần");
-            trangThaiSort.Add("Giảm dần");
-            trangThaiSort.Add("Không sắp xếp");
-
-            cboRatingValue.DataSource = trangThaiSort;
-            cboRatingValue.AllowDropDownResize = false;
-            cboSortRatingID.DataSource = trangThaiSort;
-            cboSortRatingID.AllowDropDownResize = false;
-
-
-            trangThaiXoa.Add("Đã bị xóa");
-            trangThaiXoa.Add("Chưa bị xóa");
-            trangThaiXoa.Add("Tất cả");
-            cboStateRating.DataSource = trangThaiXoa;
-
-            cboStateRating.AllowDropDownResize = false;
-
-            // Tạo dữ liệu giả mạo cho ListView
-            Random random = new Random();
-            string[] danhGia = { "Rất tốt", "Tốt", "Bình thường", "Kém", "Rất kém" };
-
-            for (int i = 1; i <= 10; i++)
-            {
-                string maDanhGia = "DG" + i.ToString("000");
-                string maDatPhong = "DP" + random.Next(1, 100).ToString("000");
-                int diemDanhGia = random.Next(1, 6);
-                string noiDungDanhGia = danhGia[random.Next(danhGia.Length)];
-                DateTime ngayDanhGia = DateTime.Today.AddDays(-random.Next(1, 30));
-
-                ListViewItem item = new ListViewItem(maDanhGia);
-                item.SubItems.Add(maDatPhong);
-                item.SubItems.Add(diemDanhGia.ToString());
-                item.SubItems.Add(noiDungDanhGia);
-                item.SubItems.Add(ngayDanhGia.ToString("dd/MM/yyyy"));
-
-                lvwRating.Items.Add(item);
-            }
-
-
-
-
-            if (lvwRating != null)
-            {
-                // Resize columns based on column header or content
-                for (int i = 0; i < lvwRating.Columns.Count; i++)
-                {
-                    int headerWidth = TextRenderer.MeasureText(lvwRating.Columns[i].Text, lvwRating.Font).Width;
-                    int contentWidth = 0;
-
-                    // Calculate the maximum width of content in the column
-                    foreach (ListViewItem item in lvwRating.Items)
-                    {
-                        int itemWidth = TextRenderer.MeasureText(item.SubItems[i].Text, lvwRating.Font).Width;
-                        contentWidth = Math.Max(contentWidth, itemWidth);
-                    }
-
-                    // Determine the width needed for the column
-                    int columnWidth = Math.Max(headerWidth, contentWidth);
-
-                    // Set the width of the column
-                    lvwRating.Columns[i].Width = columnWidth;
-                }
-            }
-
-
-
-
-
-        }
-
-    
-
         private void ucRating_Load(object sender, EventArgs e)
         {
-            khoiTaoGiaTrichoForm();
+           
+        }
+
+        private void btnAddRating_Click(object sender, EventArgs e)
+        {
+            frm.isAdd = true;
+            frm.ShowDialog();
+        }
+
+        private void btnEditRating_Click(object sender, EventArgs e)
+        {
+            if (dgvRating.SelectedRows.Count > 0)
+            {
+
+                frm.isAdd = false;
+
+
+
+
+                frm.ShowDialog();
+            }
+            else
+            {
+                thongBao = new customMessageBox("Hãy chọn một dòng dữ liệu bạn muốn chỉnh sửa!");
+                thongBao.ShowDialog();
+            }
+        }
+
+        private void btnDeleteRating_Click(object sender, EventArgs e)
+        {
+
+            if (dgvRating.SelectedRows.Count > 0)
+            {
+                thongBao = new customMessageBox("Bạn có chắc chắn muốn xóa dòng dữ liệu này không?");
+                DialogResult dr = thongBao.ShowDialog();
+                if (dr != DialogResult.Cancel)
+                {
+                    // Xóa 
+                }
+            }
+            else
+            {
+                thongBao = new customMessageBox("Hãy chọn một dòng dữ liệu bạn muốn xóa!");
+                thongBao.ShowDialog();
+            }
+        }
+
+        private void btnRecoverRating_Click(object sender, EventArgs e)
+        {
+            if (dgvRating.SelectedRows.Count > 0)
+            {
+                thongBao = new customMessageBox("Bạn có chắc chắn muốn khôi phục dòng dữ liệu này không?");
+                DialogResult dr = thongBao.ShowDialog();
+                if (dr != DialogResult.Cancel)
+                {
+                    // Khôi phục
+                }
+            }
+            else
+            {
+                thongBao = new customMessageBox("Hãy chọn một dòng dữ liệu bạn muốn khôi phục!");
+                thongBao.ShowDialog();
+            }
         }
     }
 }
