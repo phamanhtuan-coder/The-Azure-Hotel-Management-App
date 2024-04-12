16e6bd060cceb96c3a64c44a5a97f03647838683
thanhsang <thanhsang@THANHSANGCFC0>
2024-04-12 11:08:29 +0700

    Sửa NhanVienBLL, NhanVienDTO

--- BLL/NhanVienBLL.cs
+++ BLL/NhanVienBLL.cs
@@ -10,17 +10,5 @@ namespace BLL
 {
     public class NhanVienBLL
     {
-        public int MaNV { get; set; }
-        public int MaNQL { get; set; }
-        public int MaTaiKhoan { get; set; }
-        public byte[] HinhAnh { get; set; }
-        public string HoTenNV { get; set; }
-        public string SDT {  get; set; }
-        public string Email { get; set; }
-        public string CCCD { get; set; }
-        public DateTime NgaySinh { get; set; }
-        public string DiaChi { get; set; }
-        public decimal Luong { get; set; }
-        public bool TrangThai { get; set; }
     }
 }
