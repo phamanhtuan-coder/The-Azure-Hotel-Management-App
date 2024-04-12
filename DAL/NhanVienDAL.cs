8c201ffb3a03f570f9a4d597fded217abc23640a
thanhsang <thanhsang@THANHSANGCFC0>
2024-04-13 00:51:20 +0700

    Cập nhật lại code

--- DAL/NhanVienDAL.cs
+++ DAL/NhanVienDAL.cs
@@ -79,48 +79,6 @@ namespace DAL
             }
         }
 
-        public List<NhanVienDTO> LoadDSNhanVienDAL()
-        {
-            try
-            {
-                SqlConnection conn = DataProvider.KetNoiDuLieu();
-                conn.Open();
-
-                SqlCommand com = new SqlCommand("spTruyVanNhanVien", conn);
-                com.CommandType = System.Data.CommandType.StoredProcedure;
-                SqlDataReader reader = com.ExecuteReader();
-                list = new List<NhanVienDTO>();
-                while (reader.Read())
-                {
-                    NhanVienDTO nhanVien = new NhanVienDTO();
-                    
-                    nhanVien.MaNV = (int)(reader["MaNV"]);
-                    nhanVien.MaNQL = reader["MaNQL"] as int?;
-                    nhanVien.MaTaiKhoan = (int)(reader["MaTaiKhoan"]);
-                    nhanVien.HinhAnh = reader["HinhAnh"] as byte[];
-                    nhanVien.HoTenNV = reader["HoTenNV"] as string;
-                    nhanVien.SDT = reader["SDT"] as string;
-                    nhanVien.Email = reader["Email"] as string;
-                    nhanVien.CCCD = reader["CCCD"] as string;
-                    nhanVien.NgaySinh = (DateTime)reader["NgaySinh"];
-                    nhanVien.DiaChi = reader["DiaChi"] as string;
-                    nhanVien.Luong = (decimal)reader["Luong"];
-                    byte[] trangThaiBytes = (byte[])reader["TrangThai"];
-                    nhanVien.TrangThai = BitConverter.ToBoolean(trangThaiBytes, 0);
-                    nhanVien.GioiTinh = reader["GioiTinh"].ToString();
-
-                    list.Add(nhanVien);
-                }
-
-                conn.Close();
-                return list;
-            }
-            catch (Exception)
-            {
-                return new List<NhanVienDTO>();
-            }
-        }
-
         public List<NhanVienDTO> LoadIDAndNameDAL()
         {
             
