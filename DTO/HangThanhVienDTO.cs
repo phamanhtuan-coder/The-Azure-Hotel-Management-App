a8d8afa99490b0e211225fce08207e1ab52e8692
thanhsang <thanhsang@THANHSANGCFC0>
2024-04-25 19:41:53 +0700

    Thêm BLL, DAL, DTO của HangThanhVien và biến trong DTO

---
+++ DTO/HangThanhVienDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HangThanhVienDTO
+    {
+      public int MaLoaiHangThanhVien {get;set;}
+      public string TenHang {get;set;}
+      public float MucGiamGia {get;set;}
+      public bool TrangThai {get;set;}
+    }
+}
