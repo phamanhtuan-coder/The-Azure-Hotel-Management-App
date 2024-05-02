a4d83530641460572d6466630193b1fb36f8f80c
thanhsang <thanhsang@THANHSANGCFC0>
2024-05-02 11:58:14 +0700

    Thêm BLL, DAL, DTO của bảng Thue

---
+++ DTO/ThueDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ThueDTO
+    {
+      public int MaThue {get; set;}
+      public string TenThue {get; set;}
+      public float TyLeThue {get; set;}
+      public string MoTa {get; set;}
+      public bool TrangThai {get; set;}
+    }
+}
